3791273f0add5286439efdb66ef6c9034bbb7f94
GeovanaRamos <geovannna.1998@gmail.com>
2020-04-13 13:49:45 -0300

    Adding student to admin

--- web/questionnaire/admin.py
+++ web/questionnaire/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Quiz, Alternative, Questionnaire, User, Teacher
+from .models import Quiz, Alternative, Questionnaire, User, Teacher, Student
 
 admin.site.register(Questionnaire)
 admin.site.register(Quiz)
 admin.site.register(Alternative)
 admin.site.register(User)
 admin.site.register(Teacher)
+admin.site.register(Student)
