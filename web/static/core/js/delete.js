ab1d828adc86dd9b573a281aed9f80138db32519
GeovanaRamos <geovannna.1998@gmail.com>
2020-04-15 15:30:30 -0300

    Correcting delete on html and js

--- web/static/core/js/delete.js
+++ web/static/core/js/delete.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
 	var token = Cookies.get('csrftoken');
     var postData = { csrfmiddlewaretoken: token };
-    $(".delete").click(function () {
+    $(document).on( 'click' , ".delete" , function(e) {
+      e.preventDefault();
       $.ajax({
         method: 'POST',
         url: $(this).attr("data-href"),
